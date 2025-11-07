eea2af07f57edce43312180f22936d5e6352a4e7
gespel <heimbrodt@uni-potsdam.de>
2025-11-07 08:54:43 +0100

    adding enable monitor

--- atol.py
+++ atol.py
@@ -6,7 +6,10 @@ class Atol:
     def __init__(self):
         pass
 
-    def scan_for_aps(self, interface="wlan1", scan_time=5):
+    def enable_monitor_mode(self, interface="wlan1"):
+        subprocess.run(["airmon-ng", "start", interface])
+
+    def scan_for_aps(self, interface="wlan1", scan_time=1):
         prefix = "dump"
         cmd = ["airodump-ng", "--write", prefix, "--output-format", "csv", interface]
         process = subprocess.Popen(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
@@ -18,4 +21,5 @@ class Atol:
 
 
 a = Atol()
+a.enable_monitor_mode()
 a.scan_for_aps()
